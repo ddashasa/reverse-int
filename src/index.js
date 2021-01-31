97460cc3bd7cd311723f1a04c4a7e977b4ea57fe
ddashasa <77571548+ddashasa@users.noreply.github.com>
2021-01-31 18:03:00 +0400

    feat: add functions code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
-}
+		let str = '';
+
+		if (n < 0) {
+			n = Number(String(n).replace('-', ''));
+		}
+
+  		for (let i = String(n).length - 1; i >= 0; i--) {
+  			str += String(n)[i];
+  		};
+  		
+  		return Number(str)
+	}
